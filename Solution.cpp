72a8e4c7249929b0aa247760438672b6018b3f37
atoddanderson <53019797+atoddanderson@users.noreply.github.com>
2025-05-16 11:28:22 -0600

    Update Solution.cpp
    Getting Psuedo-code made

--- Solution.cpp
+++ Solution.cpp
@@ -1,26 +1,28 @@
 
 #include <iostream>
 
-int solvePuzzle(int n) {
-  int count = 0;
-  while(count < n) {
-    std::cout << "test {count}" << std::endl;
-    count++;
-  }
-}
-
 int main() {
   
-  //get test number
-  int input;
-  int numOfCells;
-  std::cout << "Enter the number for N" << std::endl;
-  std::cin >> input;
+  //Read file containing the set and target sum
+  //Use multi-processing to prune any number > target sum
+  //If the filtering/scanning process finds an element == target sum, return true
+  //Add wait function to ensure sorting doesn't start too early
   
-  //square the input
-  numOfCells = input*2;
+  //New values go into a new list to be sorted
 
-  //call the function to retrieve or create the sudoku problem to test
-  solvePuzzle(numOfCells);
+  
+  //Multi-process sorting
+  //Forward Sort
+
+  //Reverse Sort
+
+
+  //Inspect elements in list
+  //Get additive value subtracting the current index from the target sum
+  //forward search
+  //If the inspecting element > additive value, move onto checking 3 elements and so on
+  
+  //floor divide target sum by 2 to get the midpoint and start at index < mid
+  //reverse search
   return 0;
 }
