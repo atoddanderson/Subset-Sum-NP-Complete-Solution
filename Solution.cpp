304fdd607836fc296ad4dcfd057d9f0646cbb459
atoddanderson <53019797+atoddanderson@users.noreply.github.com>
2025-05-17 09:56:18 -0600

    Update Solution.cpp
    Pseudo-code updates

--- Solution.cpp
+++ Solution.cpp
@@ -7,22 +7,20 @@ int main() {
   //Use multi-processing to prune any number > target sum
   //If the filtering/scanning process finds an element == target sum, return true
   //Add wait function to ensure sorting doesn't start too early
-  
-  //New values go into a new list to be sorted
+  //None pruned values go into a new list to be sorted
 
   
   //Multi-process sorting
   //Forward Sort
-
   //Reverse Sort
 
 
+  //Multi-process searching
   //Inspect elements in list
   //Get additive value subtracting the current index from the target sum
-  //forward search
-  //Start Search on index after mid point
+  //floor divide target sum by 2 to get the midpoint
+  //Start Forward Search on index after mid point
   //If the inspecting element > additive value, move onto checking 3 elements and so on
-  
   //floor divide target sum by 2 to get the midpoint and start at index < mid
   //reverse search
   return 0;
